fc83b5ef6713ddf84a257e703507d367bd4f7cd1
Andrea Lopez Palomeque <andrealopezpalomeque@gmail.com>
2023-12-25 22:14:45 -0300

    matrices

--- src/1-matrices/main.go
+++ src/1-matrices/main.go
@@ -18,8 +18,28 @@ func main() {
 
 	//cuando no se la cantidad de datos que va a tener la matriz
 	c := [...]int{1,2,3} // [1 2 3]
+
+	//recorrer una matriz con FOR
+	for i := 0; i < len(c); i++ {
+		fmt.Println(c[i])
+	}
+
+	//recorrer una matriz con RANGE //index, value
+	for i, v := range c {
+		//fmt.Println(i, v) //devuelve el index y el valor
+		fmt.Printf("indice: %d, valor: %d\n", i, v)
+	}
+
+	//! matrices BI-DIMENSIONALES
+
+	var matrix = [3][2]int{
+		{1,2},
+		{3,4},
+		{5,6},
+	}
+
+	fmt.Println(matrix) // [[1 2] [3 4] [5 6]]
+
+
 	
-	fmt.Println(a)
-	fmt.Println(b)
-	fmt.Println(c)
 }
